f41290c4c508b1bc24327dd5664e37d35ded7e19
Rengkat <successinmotion11@gmail.com>
2025-08-15 01:51:21 +0100

    profile service added

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,10 +1,27 @@
+import { Repository } from 'typeorm';
+import { createProfileDto } from './dtos/creatProfile.dto';
+import { Profile } from './profile.Entity';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProfileService {
-  getProfile() {}
-  getCurrentUserProfile() {}
-  createProfile() {}
-  updateProfile() {}
-  updateCurrentUserProfile() {}
+  constructor(
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+  ) {}
+
+  async getProfile(userId: number) {
+    return this.profileRepository.findOne({ where: { user: { id: userId } } });
+  }
+
+  async createProfile(profileDto: createProfileDto) {
+    const profile = this.profileRepository.create(profileDto);
+    return this.profileRepository.save(profile);
+  }
+
+  async updateProfile(id: number, updateData: Partial<Profile>) {
+    await this.profileRepository.update(id, updateData);
+    return this.profileRepository.findOneBy({ id });
+  }
 }
