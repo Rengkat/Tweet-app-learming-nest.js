9b0a1952d7f4a5edf234c78828125e860d0f96f2
Rengkat <successinmotion11@gmail.com>
2025-08-30 09:08:36 +0100

    creating page links

--- src/common/pagination/pagination.provider.ts
+++ src/common/pagination/pagination.provider.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PaginationQueryDto } from '../PaginationQueryDto';
 import { FindManyOptions, FindOptionsWhere, ObjectLiteral, Repository } from 'typeorm';
+import { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
 // we are creating Module and Interface to be able to 
 // use pagination in all the module. I t will be like a cetral place
 
 //provider is similar as service but this is to inject it into another service
 @Injectable()
 export class PaginationProvider {
+    constructor(@Inject(REQUEST) private readonly request:Request){}//for links page url
     public async paginationQuery<T extends ObjectLiteral>(
         paginationQueryDto:PaginationQueryDto,
         //we need the reposotory of what we want to paginate, since is generic we use a general name
@@ -21,6 +24,30 @@ export class PaginationProvider {
         if (where) {
             findOptions.where=where
         }
-        return await repository.find(findOptions)
+        const result = await repository.find(findOptions)
+        const totalItems= await repository.count()
+        const totalPage = Math.ceil(totalItems/paginationQueryDto.limit)
+        const nextPage= paginationQueryDto.page===totalPage?
+        paginationQueryDto.page:paginationQueryDto.page++
+        const previousPage =paginationQueryDto.page<1? 1:paginationQueryDto.page
+        const baseUrl = `${this.request.protocol}://${this.request.headers.host}/`
+        const newUrl = new URL(this.request.url, baseUrl)
+        const response ={
+            data:result,
+    meta:{
+        itemPerFage:paginationQueryDto.limit,
+        totalItems:totalItems,
+        currentPage:paginationQueryDto.page,
+        totalPages:totalPage
+    },
+    links:{
+        first:`${newUrl.origin}${newUrl.pathname}?limit=${paginationQueryDto.limit}&page=1`,
+        last:`${newUrl.origin}${newUrl.pathname}?limit=${paginationQueryDto.limit}&page=${totalPage}`,
+        current:`${newUrl.origin}${newUrl.pathname}?limit=${paginationQueryDto.limit}&page=${paginationQueryDto.page}`,
+        previous:`${newUrl.origin}${newUrl.pathname}?limit=${paginationQueryDto.limit}&page=${previousPage}`,
+        next:`${newUrl.origin}${newUrl.pathname}?limit=${paginationQueryDto.limit}&page=${nextPage}`
+    }
+        }
+        return result
     }
 }
